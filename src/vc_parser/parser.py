506b3f70cdd64fa38f683a62eae80561725dfe36
gt-pg1 <shot66766@gmail.com>
2022-11-22 16:38:29 +0400

    Added help texts for command line commands

--- src/vc_parser/parser.py
+++ src/vc_parser/parser.py
@@ -4,23 +4,45 @@ from typing import NoReturn
 import data_collector
 
 
-def create_cl_parser():
-    parser_cl = argparse.ArgumentParser()
-    subparsers_cl = parser_cl.add_subparsers(dest='command')
+def create_command_line_parser():
+    parser_cl = argparse.ArgumentParser(prog='VC and DTF parser')
+    subparsers_cl = parser_cl.add_subparsers(
+        dest='source', title='Possible commands', description='Commands must be entered as the first parameter'
+    )
 
     vc_parser = subparsers_cl.add_parser('vc')
-    vc_parser.add_argument('-a', '--articles', type=int, default=1000)
-    vc_parser.add_argument('-aid', '--article_id', type=int)
-    vc_parser.add_argument('-d', '--delay', type=float, default=0.5)
-    vc_parser.add_argument('--output_csv', type=bool, default=False)
-    vc_parser.add_argument('--output_json', type=bool, default=False)
+    vc_parser.add_argument(
+        '-a', '--articles', type=int, default=1000, help='(int) Set number of articles to collect'
+    )
+    vc_parser.add_argument(
+        '-aid', '--article_id', type=int, help='(int) Set ID from which the collection will start or a specific article'
+    )
+    vc_parser.add_argument(
+        '-d', '--delay', type=float, default=0.5, help='(float) Set the delay between requests to the server'
+    )
+    vc_parser.add_argument(
+        '-csv', '--output_csv', type=bool, default=False, help='(bool) Generate .csv file with result'
+    )
+    vc_parser.add_argument(
+        '-json', '--output_json', type=bool, default=False, help='(bool) Generate .json file with result'
+    )
 
     dtf_parser = subparsers_cl.add_parser('dtf')
-    dtf_parser.add_argument('-a', '--articles', type=int, default=1000)
-    dtf_parser.add_argument('-aid', '--article_id', type=int)
-    dtf_parser.add_argument('-d', '--delay', type=float, default=0.5)
-    dtf_parser.add_argument('--output_csv', type=bool, default=False)
-    dtf_parser.add_argument('--output_json', type=bool, default=False)
+    dtf_parser.add_argument(
+        '-a', '--articles', type=int, default=1000, help='(int) Set number of articles to collect'
+    )
+    dtf_parser.add_argument(
+        '-aid', '--article_id', type=int, help='(int) Set ID from which the collection will start or a specific article'
+    )
+    dtf_parser.add_argument(
+        '-d', '--delay', type=float, default=0.5, help='(float) Set the delay between requests to the server'
+    )
+    dtf_parser.add_argument(
+        '-csv', '--output_csv', type=bool, default=False, help='(bool) Generate .csv file with result'
+    )
+    dtf_parser.add_argument(
+        '-json', '--output_json', type=bool, default=False, help='(bool) Generate .json file with result'
+    )
 
     return parser_cl
 
@@ -51,12 +73,12 @@ def run_dtf(namespace):
 
 
 def main() -> NoReturn:
-    parser_cl = create_cl_parser()
+    parser_cl = create_command_line_parser()
     namespace = parser_cl.parse_args(sys.argv[1:])
 
-    if namespace.command == 'vc':
+    if namespace.source == 'vc':
         run_vc(namespace)
-    elif namespace.command == 'dtf':
+    elif namespace.source == 'dtf':
         run_dtf(namespace)
     else:
         print('Specify source ("vc" or "dtf")')
